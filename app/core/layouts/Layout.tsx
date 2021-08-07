ccea81ab035ecaa5abfcf76f414d39fb4250b1ee
linsheng24 <linsheng24@gmail.com>
2021-08-07 20:07:33 +0800

    add navbar

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,19 +1,70 @@
-import { ReactNode } from "react"
-import { Head } from "blitz"
+import React, { ReactNode } from "react"
+import { Head, Image, Routes, Link } from "blitz"
+import { Grid, makeStyles } from "@material-ui/core"
+import styled from "styled-components"
 
 type LayoutProps = {
   title?: string
   children: ReactNode
 }
 
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    backgroundColor: theme.palette.primary.light,
+  },
+  logo: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}))
+
+const StyledLink = styled.a`
+  padding: 10px;
+  font-size: 15px;
+  text-decoration: none;
+  border-radius: 5px;
+  margin-right: 5px;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+`
+
+const LoginLink = styled(StyledLink)`
+  margin-left: auto;
+`
+
 const Layout = ({ title, children }: LayoutProps) => {
+  const classes = useStyles()
   return (
     <>
       <Head>
-        <title>{title || "mylender"}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>MyLender</title>
       </Head>
-
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        className={classes.navbar}
+      >
+        <Grid item container sm={8} xs={12}>
+          <Grid className={classes.logo} xs={4}>
+            <Image src="/logo.png" alt="Picture of the author" width={130} height={50} />
+          </Grid>
+          <Grid item container alignItems="center" xs={8}>
+            <Link href={Routes.Home()} passHref>
+              <StyledLink>我要出借</StyledLink>
+            </Link>
+            <Link href={Routes.Home()} passHref>
+              <StyledLink>查看產品</StyledLink>
+            </Link>
+            <Link href={Routes.Home()} passHref>
+              <LoginLink>登入</LoginLink>
+            </Link>
+          </Grid>
+        </Grid>
+      </Grid>
       {children}
     </>
   )
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -9,16 +9,36 @@ import {
 } from "blitz"
 import LoginForm from "app/auth/components/LoginForm"
 
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider, createTheme } from "@material-ui/core/styles"
+import React from "react"
+import { green, purple } from "@material-ui/core/colors"
+
+//You can customize this as you want and even move it out to a separate file
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[200],
+    },
+    secondary: {
+      main: purple[400],
+    },
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <ErrorBoundary
-      FallbackComponent={RootErrorFallback}
-      onReset={useQueryErrorResetBoundary().reset}
-    >
-      {getLayout(<Component {...pageProps} />)}
-    </ErrorBoundary>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ErrorBoundary
+        FallbackComponent={RootErrorFallback}
+        onReset={useQueryErrorResetBoundary().reset}
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </ErrorBoundary>
+    </ThemeProvider>
   )
 }
 
